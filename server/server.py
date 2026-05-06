f32dd40c01c8b789f3792754d7c6ca6c2c03bd0b
taose4874 <taose4874@users.noreply.github.com>
2026-05-06 00:51:12 +0000

    fix: add common module to server for packaging

--- server/server.py
+++ server/server.py
@@ -5,7 +5,9 @@ import sys
 import os
 import json
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+if os.path.exists(os.path.join(os.path.dirname(__file__), 'common')):
+    sys.path.insert(0, os.path.dirname(__file__))
+
 from common import Message, MessageType
 
 class RemoteDesktopServer:
@@ -17,7 +19,7 @@ class RemoteDesktopServer:
         self.running = False
         self.socket = None
         self.lock = threading.Lock()
-    
+
     def start(self):
         self.socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -25,7 +27,7 @@ class RemoteDesktopServer:
         self.socket.listen(10)
         self.running = True
         print(f"服务器启动成功，监听 [{self.host}]:{self.port}")
-        
+
         try:
             while self.running:
                 client_socket, client_address = self.socket.accept()
@@ -39,7 +41,7 @@ class RemoteDesktopServer:
         except KeyboardInterrupt:
             print("\n服务器正在关闭...")
             self.stop()
-    
+
     def stop(self):
         self.running = False
         with self.lock:
@@ -52,7 +54,7 @@ class RemoteDesktopServer:
         if self.socket:
             self.socket.close()
         print("服务器已关闭")
-    
+
     def handle_client(self, client_socket, client_address):
         client_id = f"{client_address[0]}:{client_address[1]}"
         try:
@@ -63,39 +65,38 @@ class RemoteDesktopServer:
                     'username': f"User_{len(self.clients) + 1}",
                     'group': None
                 }
-            
+
             self.broadcast_user_list()
-            
+
             buffer = b''
             while self.running:
                 data = client_socket.recv(65536)
                 if not data:
                     break
                 buffer += data
-                
+
                 while len(buffer) >= Message.get_header_size():
-                    # 解析头部获取消息类型和payload长度
                     msg_type, payload_len = Message.parse_header(buffer)
-                    
+
                     if len(buffer) < Message.get_header_size() + payload_len:
                         break
-                    
+
                     msg = Message.deserialize(buffer[:Message.get_header_size() + payload_len])
                     buffer = buffer[Message.get_header_size() + payload_len:]
-                    
+
                     self.process_message(client_id, msg)
-        
+
         except Exception as e:
             print(f"客户端 {client_id} 错误: {e}")
         finally:
             self.remove_client(client_id)
-    
+
     def process_message(self, client_id: str, msg: Message):
         with self.lock:
             client = self.clients.get(client_id)
             if not client:
                 return
-        
+
         if msg.msg_type == MessageType.JOIN_GROUP:
             group_name = msg.data.get('group', 'default')
             username = msg.data.get('username')
@@ -108,7 +109,7 @@ class RemoteDesktopServer:
                 if client_id not in self.groups[group_name]:
                     self.groups[group_name].append(client_id)
             self.broadcast_user_list()
-        
+
         elif msg.msg_type == MessageType.LEAVE_GROUP:
             group_name = client.get('group')
             with self.lock:
@@ -117,7 +118,7 @@ class RemoteDesktopServer:
                         self.groups[group_name].remove(client_id)
                 client['group'] = None
             self.broadcast_user_list()
-        
+
         elif msg.msg_type == MessageType.SCREEN_FRAME:
             target_id = msg.data.get('target')
             if target_id and target_id in self.clients:
@@ -125,7 +126,7 @@ class RemoteDesktopServer:
                     self.clients[target_id]['socket'].send(msg.serialize())
                 except:
                     pass
-        
+
         elif msg.msg_type in (MessageType.MOUSE_EVENT, MessageType.KEYBOARD_EVENT):
             target_id = msg.data.get('target')
             if target_id and target_id in self.clients:
@@ -133,13 +134,13 @@ class RemoteDesktopServer:
                     self.clients[target_id]['socket'].send(msg.serialize())
                 except:
                     pass
-        
+
         elif msg.msg_type == MessageType.HEARTBEAT:
             try:
                 client_socket.send(msg.serialize())
             except:
                 pass
-    
+
     def broadcast_user_list(self):
         with self.lock:
             user_list = []
@@ -149,7 +150,7 @@ class RemoteDesktopServer:
                     'username': info['username'],
                     'group': info['group']
                 })
-        
+
         msg = Message(MessageType.USER_LIST, {'users': user_list})
         with self.lock:
             for client_id in list(self.clients.keys()):
@@ -157,7 +158,7 @@ class RemoteDesktopServer:
                     self.clients[client_id]['socket'].send(msg.serialize())
                 except:
                     self.remove_client(client_id)
-    
+
     def remove_client(self, client_id: str):
         with self.lock:
             if client_id in self.clients:
